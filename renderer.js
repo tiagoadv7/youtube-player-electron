24ec762998533a133a6e26553350f9c8aae5c96c
Tiago Lima <51231966+tiagoadv7@users.noreply.github.com>
2024-09-10 09:59:51 -0300

    ajuste no campo do input limpar o campo apos irserção de url invalida ou texto

--- renderer.js
+++ renderer.js
@@ -10,17 +10,19 @@ function isValidYouTubeUrl(url) {
 document.getElementById('playBtn').addEventListener('click', () => {
   const urlInput = document.getElementById('urlInput').value.trim();
 
-  // Verifica se a URL está vazia
-  if (!urlInput) {
-    displayError('Por favor, insira uma URL.');
-    return;
-  }
+ // Verifica se a URL está vazia
+ if (!urlInput) {
+  displayError('Por favor, insira uma URL.');
+  return;
+}
 
-  // Verifica se a URL é válida (do YouTube)
-  if (!isValidYouTubeUrl(urlInput)) {
-    displayError('Insira uma URL válida do YouTube.');
-    return;
-  }
+// Verifica se a URL é válida (do YouTube)
+if (!isValidYouTubeUrl(urlInput)) {
+  displayError('Insira uma URL válida do YouTube.');
+  // Limpa o campo de input após inserir uma URL inválida
+  document.getElementById('urlInput').value = '';
+  return;
+}
 
   // Envia a URL para o processo principal se estiver preenchida e válida
   ipcRenderer.send('play-video', urlInput);
@@ -29,15 +31,24 @@ document.getElementById('playBtn').addEventListener('click', () => {
   document.getElementById('urlInput').value = '';
 });
 
-// Função para exibir mensagens de erro
+// Função para exibir mensagens de erro com fade
 function displayError(message) {
   const errorElement = document.getElementById('error-alert');
   errorElement.textContent = message;
-  errorElement.style.display = 'block';
 
-  // Remove o alerta após 3 segundos
+  // Mostra o alerta com fade-in
+  errorElement.style.display = 'block';
+  errorElement.style.opacity = 0;
   setTimeout(() => {
-    errorElement.style.display = 'none';
+    errorElement.style.opacity = 1;
+  }, 100);
+
+  // Remove o alerta após 3 segundos com fade-out
+  setTimeout(() => {
+    errorElement.style.opacity = 0;
+    setTimeout(() => {
+      errorElement.style.display = 'none';
+    }, 700);  // Tempo do fade-out
   }, 3000);
 }
 
